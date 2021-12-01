7d5e44edf93d16d87922fc4f024bb61a7ae0c6c2
Sam Anderson <sam@andomain.co.uk>
2021-12-01 23:21:49 +0000

    Configure eslint/prettier

--- src/2021/day1.ts
+++ src/2021/day1.ts
@@ -1,17 +1,15 @@
 const parseInput = (input: string[]) => input.map(Number);
 
 function part1(input: Array<string>) {
-  let [current, ...depths] = parseInput(input);
-  let score = 0;
+  const parsedInput = parseInput(input);
 
-  for (let reading of depths) {
-    if (reading > current) {
-      score++;
+  return parsedInput.reduce((score, depth, idx) => {
+    if (idx === 0 || depth < parsedInput[idx - 1]) {
+      return score;
     }
-    current = reading;
-  }
 
-  return score;
+    return score + 1;
+  }, 0);
 }
 
 function part2(input: Array<string>) {
@@ -20,10 +18,10 @@ function part2(input: Array<string>) {
 
   const parsedInput = parseInput(input);
 
-  for (let index = 0; index < input.length - 3; index++) {
+  for (let index = 0; index < input.length - 3; index += 1) {
     const nextDepth = parsedInput[index] + parsedInput[index + 1] + parsedInput[index + 2];
     if (nextDepth > currentDepth) {
-      score++;
+      score += 1;
     }
     currentDepth = nextDepth;
   }
@@ -31,7 +29,4 @@ function part2(input: Array<string>) {
   return score;
 }
 
-export {
-  part1,
-  part2
-}
+export { part1, part2 };
