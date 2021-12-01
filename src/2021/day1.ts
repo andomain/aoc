959b351e29ecb3808ccb56cb4e43e4b2da2ab7fa
Sam Anderson <sam@andomain.co.uk>
2021-12-01 17:45:20 +0000

    2021 Day 1

---
+++ src/2021/day1.ts
@@ -0,0 +1,36 @@
+const parseInput = (input: string[]) => input.map(Number);
+function part1(input: Array<string>) {
+  let [current, ...depths] = parseInput(input);
+  let score = 0;
+
+  for (let reading of depths) {
+    if (reading > current) {
+      score++;
+    }
+    current = reading;
+  }
+
+  return score;
+}
+
+function part2(input: Array<string>) {
+  let currentDepth = 0;
+  let score = 0;
+
+  const parsedInput = parseInput(input);
+
+  for (let index = 0; index < input.length - 3; index++) {
+    const nextDepth = parsedInput[index] + parsedInput[index + 1] + parsedInput[index + 2];
+    if (nextDepth > currentDepth) {
+      score++;
+    }
+    currentDepth = nextDepth;
+  }
+
+  return score;
+}
+
+export {
+  part1,
+  part2
+}
